5c913b8e5fbd680f7a45f1406767ea3f2833a0bb
natch2005 <36702177+natch2005@users.noreply.github.com>
2018-08-05 15:08:08 +0700

    Update index.php

--- index.php
+++ index.php
@@ -18,7 +18,8 @@
       $arrayPostData['messages'][1]['packageId'] = "2";
       $arrayPostData['messages'][1]['stickerId'] = "34";
      
-      header('Location:http://1.179.171.188/outsource/linelogin.php?mes=5555');
+      
+      navigate('http://1.179.171.188/outsource/linelogin.php?mes=5555'); 
       pushMsg($arrayHeader,$arrayPostData);
       
    }
